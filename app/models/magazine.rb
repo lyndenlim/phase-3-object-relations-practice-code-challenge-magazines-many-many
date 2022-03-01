5ba9d90167d9331c67d288c1b1d2eaf491579444
Lynden Lim <lyndenlim15@gmail.com>
2022-03-01 16:22:49 -0500

    Completed

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,12 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list
+    self.readers.map { |reader| reader.email }.join(";")
+  end
+
+  def self.most_popular
+    Magazine.all.max_by { |magazine| magazine.subscriptions.count }
+  end
 end
